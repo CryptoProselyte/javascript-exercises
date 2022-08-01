c673980b6b8d6e12a0dd3c3491d90524a92abca2
CryptoProselyte <107948381+CryptoProselyte@users.noreply.github.com>
2022-08-01 21:47:54 +0430

    Update fibonacci.js

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,14 +1,22 @@
-const fibonacci = function(count) {
-  if (count < 0) return "OOPS";
-  if (count === 0) return 0;
-  let a = 0;
-  let b = 1;
-  for (let i = 1; i < count; i++) {
-    const temp = b;
-    b = a + b;
-    a = temp;
-  }
-  return b;
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS"
+    }
+    if (typeof n == "string") {
+
+     n = Number(n);
+
+    }
+let fibonaccinumber = [];
+fibonaccinumber[0] = 1;
+fibonaccinumber[1] = 1;
+
+for (let i = 2; i < n; i++) {
+    fibonaccinumber[i] = fibonaccinumber[i-1] + fibonaccinumber[i-2];
+    
+}
+return fibonaccinumber[n-1];
 };
+
 
 module.exports = fibonacci;
